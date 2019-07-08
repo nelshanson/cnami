6a72a7cb7e2f21508cbf8b427153a93309c612d7
Nels Hanson <nelshanson@gmail.com>
2019-07-07 23:42:22 -0400

    Linked libm, implemented simple advection

--- src/cnami.c
+++ src/cnami.c
@@ -1,9 +1,49 @@
 #include <stdio.h>
+#include <math.h>
 #include "CnamiConfig.h"
 
 int main(int argc, char **argv) {
-  // Simulate a tsunami.
-  printf("Tsunami!!!\n");
+  
+  const long im = 100;
+  const long nm = 100;
+
+  const double dt = 1.;
+  const double dx = 1.;
+  const double c = 1.;
+
+  double du[im];
+  double u[im];
+
+  const long ipos = 25;
+  const double decay = 0.02;
+
+  for (size_t i = 0; i < im; ++i) {
+    double x = (i - ipos + 1) * (i - ipos + 1);
+    u[i] = exp(-decay * x);
+  }
+
+  for (size_t i = 0; i < im; ++i) {
+    printf("%f ", u[i]);
+  }
+  printf("\n");
+
+  for (size_t n = 0; n < nm; ++n) {
+    du[0] = u[0] - u[im-1];
+
+    for (size_t i = 1; i < im; ++i) {
+      du[i] = u[i] - u[i-1];
+    }
+
+    for (size_t i = 0; i < im; ++i) {
+      u[i] = u[i] - c * du[i] / dx * dt;
+    }
+
+    for (size_t i = 0; i < im; ++i) {
+      printf("%f ", u[i]);
+    }
+    printf("\n");
+
+  }
 
   return 0;
 }
